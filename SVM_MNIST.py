a5dc75696bfc5f68d3965fa191f09a52d4fd5bb9
ahiller76 <79170607+ahiller76@users.noreply.github.com>
2021-05-17 18:06:40 -0400

    Add files via upload

--- SVM_MNIST.py
+++ SVM_MNIST.py
@@ -1,28 +1,25 @@
-# Use the support vector machine algorithm to classify the data points into three classes
+### Aaron Hiller, Kit Sloan
 
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn import svm, datasets
+from sklearn import svm
 from keras.datasets import mnist
 
-(train_image, train_labels), (test_image, test_labels) = mnist.load_data()
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
-# reshape and scale the training and test samples
-train_images = train_image.reshape((60000, 28 * 28)).astype('float32') / 255.0
-test_images = test_image.reshape((10000, 28 * 28)).astype('float32') / 255.0
+train_data = np.reshape(train_images, (60000, 28*28))
+test_data = np.reshape(test_images, (10000, 28*28))
 
-# X include 60000 data points, X.shape is (60000,734)
-X = train_images
-Y = train_labels    # Y.shape is (60000,1)
+X_train = train_data/255.0
+Y_train = train_labels
+model = svm.SVC(kernel = 'rbf', C = 5, gamma = 0.005)
+model.fit(X_train, Y_train)
+score = model.score(X_train, Y_train)
+print('Training Accuracy', score*100, '%')
 
-# We create an instance of SVM and fit out data.
-SVM = svm.SVC(kernel='rbf', C=5, gamma=.005).fit(X, Y)
-train_acc = SVM.score(X, Y)   # Returns the mean accuracy on the given test data and labels.
-print('Training Accuracy:', train_acc*100, '%')
+X_test = test_data/255.0
+Y_test = test_labels
+Y_predict = model.predict(X_test)
+model.fit(X_test, Y_predict)
+score_pred = model.score(X_test, Y_test)
+print('Test Accuracy', score_pred*100, '%')
 
-
-# use the trained model to predict the classification of the data points in the mesh.
-Z = SVM.predict(test_images)
-SVM.fit(test_images, Z)
-test_acc = SVM.score(test_images, test_labels)
-print('Test Accuracy:', test_acc*100, '%')
